ab3f27fd2fb32a52f76146970823525f044a325c
dacctal <dacctal@symlinx.net>
2026-06-05 20:34:30 +0000

    dependency resolution & installation is fully working, also bldit has luajit dependency now.

--- src/vars.c
+++ src/vars.c
@@ -48,7 +48,7 @@ const char* get_install_dir(const char *key) {
     return "";
 }
 
-const char *version = "0.0.1"; //changed from "0.0.0" muhahahah
+const char *version = "0.1.0";
 
 const char *red = "\e[0;31m";
 const char *green = "\e[0;32m";
